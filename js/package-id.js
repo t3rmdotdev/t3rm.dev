08179230b17f29fd12a75a14c8aa8bfd3772521a
t3rmdotdev <89753866+t3rmdotdev@users.noreply.github.com>
2021-09-20 20:53:07 -0700

    Add files via upload

---
+++ js/package-id.js
@@ -0,0 +1,15 @@
+window.addEventListener(
+  "message",
+  (event) => {
+    // if (event.origin !== "https://t3rm.dev") return;
+
+    if (event.data.slice(0, 16) === "t3rm:package-id:") {
+      const packageID = document.getElementById("package-id");
+      if (packageID.style.display === "block") return;
+
+      packageID.style.display = "block";
+      packageID.innerText = "dev.t3rm." + event.data.slice(16);
+    }
+  },
+  false
+);
