1ee0cf449cb14a5d7404715c268cb6a801ee1a31
t3rmdotdev <89753866+t3rmdotdev@users.noreply.github.com>
2021-09-20 20:59:53 -0700

    Check data exists

--- js/package-id.js
+++ js/package-id.js
@@ -3,7 +3,7 @@ window.addEventListener(
   (event) => {
     // if (event.origin !== "https://t3rm.dev") return;
 
-    if (event.data.slice(0, 16) === "t3rm:package-id:") {
+    if (event.data && event.data.slice(0, 16) === "t3rm:package-id:") {
       const packageID = document.getElementById("package-id");
       if (packageID.style.display === "block") return;
 
